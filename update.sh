cab6fe52d3c087bc6c0dcb42fd50f562f9b4a1b6
xwall60 <xwall@163.com>
2025-07-25 14:00:02 +0800

    Update update.sh
    Signed-off-by: xwall60 <xwall@163.com>

--- update.sh
+++ update.sh
@@ -163,7 +163,7 @@ install_small8() {
         tuic-client chinadns-ng ipt2socks tcping trojan-plus simple-obfs shadowsocksr-libev \
         luci-app-passwall smartdns luci-app-smartdns v2dat mosdns luci-app-mosdns \
         adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go taskd luci-lib-xterm luci-lib-taskd \
-        luci-app-cloudflarespeedtest \
+        luci-app-cloudflarespeedtest luci-app-rtbwmon luci-app-wechatpush\
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
         msd_lite luci-app-msd_lite cups luci-app-cupsd
